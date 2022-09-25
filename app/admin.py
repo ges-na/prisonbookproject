c91052b35c49b6fcc1feab38ca3f03a5e4fcd35b
ges-na <gesina.a.phillips@gmail.com>
2022-09-25 15:19:15 -0400

    making updates based on feedback

--- app/admin.py
+++ app/admin.py
@@ -1,33 +1,40 @@
 import datetime
-import json
 
+from ajax_select import make_ajax_form
+from ajax_select.fields import autoselect_fields_check_can_add
 from django.contrib import admin
-from django.db.models import F
 from django.forms import ModelForm
 from django.urls import reverse
 from django.utils.html import format_html
-
-from import_export import resources
+from import_export import resources, widgets
 from import_export.admin import ImportExportModelAdmin
 from import_export.fields import Field
-from import_export import widgets
-from ajax_select import make_ajax_form
-from ajax_select.fields import autoselect_fields_check_can_add
 
-from .models import Person, Prison, Letter, PersonPrison, WorkflowStage
+from .models import Letter, Person, PersonPrison, Prison, WorkflowStage
 
-#probably only good for testing
-@admin.action(description='Mark selected letters as Processed')
-def move_to_processed(modeladmin, request, queryset):
-    queryset.update(awaiting_fulfillment_date=None, fulfilled_date=None, workflow_stage=WorkflowStage.PROCESSED)
 
-#should this blank fulfilled_date?
-@admin.action(description='Mark selected letters as Awaiting Fulfillment')
+# probably only good for testing
+@admin.action(description="Mark selected letters as Stage 1 Complete")
+def move_to_stage1_complete(modeladmin, request, queryset):
+    queryset.update(
+        awaiting_fulfillment_date=None,
+        fulfilled_date=None,
+        workflow_stage=WorkflowStage.STAGE1_COMPLETE,
+    )
+
+
+# should this blank fulfilled_date?
+@admin.action(description="Mark selected letters as Awaiting Fulfillment")
 def move_to_awaiting_fulfillment(modeladmin, request, queryset):
     now = datetime.datetime.now()
-    queryset.update(awaiting_fulfillment_date=now, fulfilled_date=None, workflow_stage=WorkflowStage.AWAITING_FULFILLMENT)
+    queryset.update(
+        awaiting_fulfillment_date=now,
+        fulfilled_date=None,
+        workflow_stage=WorkflowStage.AWAITING_FULFILLMENT,
+    )
 
-@admin.action(description='Mark selected letters as Fulfilled')
+
+@admin.action(description="Mark selected letters as Fulfilled")
 def move_to_fulfilled(modeladmin, request, queryset):
     now = datetime.datetime.now()
     for obj in queryset:
@@ -37,20 +44,33 @@ def move_to_fulfilled(modeladmin, request, queryset):
 
 
 class PersonResource(resources.ModelResource):
-    legacy_last_served_date = Field(attribute='legacy_last_served_date', column_name='legacy_last_served_date', widget=widgets.DateWidget(format='%m/%d/%Y'))
+    legacy_last_served_date = Field(
+        attribute="legacy_last_served_date",
+        column_name="legacy_last_served_date",
+        widget=widgets.DateWidget(format="%m/%d/%Y"),
+    )
 
     class Meta:
         model = Person
         skip_unchanged = True
         report_skipped = True
-        fields = ('id', 'inmate_number', 'last_name', 'first_name', 'legacy_prison_id', 'legacy_last_served_date')
+        fields = (
+            "id",
+            "inmate_number",
+            "last_name",
+            "first_name",
+            "legacy_prison_id",
+            "legacy_last_served_date",
+        )
 
     def after_save_instance(self, instance, using_transactions, dry_run):
         legacy_prison_id = instance.legacy_prison_id
         prison = Prison.objects.filter(legacy_id=legacy_prison_id).first()
         if not prison:
             raise Exception(f"Prison with legacy id {legacy_prison_id} not found!")
-        existing = PersonPrison.objects.filter(person_id=instance.id, prison_id=prison.id).first()
+        existing = PersonPrison.objects.filter(
+            person_id=instance.id, prison_id=prison.id
+        ).first()
         if existing and existing.current:
             return
         elif existing:
@@ -58,41 +78,58 @@ class PersonResource(resources.ModelResource):
             # PersonPrison.objects.filter(person_id=instance.id).update(current=False)
             existing.save()
             return
-        PersonPrison.objects.create(person_id=instance.id, prison_id=prison.id, current=True)
+        PersonPrison.objects.create(
+            person_id=instance.id, prison_id=prison.id, current=True
+        )
 
 
 class PrisonResource(resources.ModelResource):
-
-
     class Meta:
         model = Prison
         skip_unchanged = True
         report_skipped = True
-        fields = ('id', 'name', 'prison_type', 'legacy_id', 'legacy_address', 'mailing_address', 'mailing_city', 'mailing_state', 'mailing_zipcode', 'restrictions', 'notes')
+        fields = (
+            "id",
+            "name",
+            "prison_type",
+            "legacy_id",
+            "legacy_address",
+            "mailing_address",
+            "mailing_city",
+            "mailing_state",
+            "mailing_zipcode",
+            "restrictions",
+            "notes",
+        )
 
 
 class PersonPrisonForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super(PersonPrisonForm, self).__init__(*args, **kwargs)
         if self.instance.pk:
-            self.fields['prison'].disabled = True
+            self.fields["prison"].disabled = True
 
 
 class PersonPrisonInline(admin.TabularInline):
     model = PersonPrison
     extra = 0
     can_delete = False
-    fields = ('prison', 'current')
+    fields = ("prison", "current")
     form = PersonPrisonForm
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        if db_field.name == "prison": kwargs["queryset"] = Prison.objects.all().order_by('name')
-        return super(PersonPrisonInline, self).formfield_for_foreignkey(db_field, request, **kwargs)
+        if db_field.name == "prison":
+            kwargs["queryset"] = Prison.objects.all().order_by("name")
+        return super(PersonPrisonInline, self).formfield_for_foreignkey(
+            db_field, request, **kwargs
+        )
 
 
 class PersonForm(ModelForm):
     def clean_inmate_number(self):
-        cleaned_inmate_num = "".join(list(filter(str.isalnum, self.cleaned_data["inmate_number"])))
+        cleaned_inmate_num = "".join(
+            list(filter(str.isalnum, self.cleaned_data["inmate_number"]))
+        )
         self.cleaned_data["inmate_number"] = cleaned_inmate_num.upper()
         return self.cleaned_data["inmate_number"]
 
@@ -111,19 +148,61 @@ class PersonAdmin(ImportExportModelAdmin):
 
     form = PersonForm
 
-    list_display = ('id', 'inmate_number', 'last_name', 'first_name', 'eligibility', 'status', 'last_served', 'current_prison', 'package_count', 'pending_letter_count', 'letter_count', 'created_by', 'created_date', 'modified_date')
-    readonly_fields = ('current_prison',)
+    list_display = (
+        "id",
+        "inmate_number",
+        "last_name",
+        "first_name",
+        "eligibility",
+        "status",
+        "last_served",
+        "current_prison",
+        "package_count",
+        "pending_letter_count",
+        "letter_count",
+        "created_by",
+        "created_date",
+        "modified_date",
+    )
+    readonly_fields = ("current_prison",)
     # list_filter = ('prisons__prison',)
-    search_fields = ('inmate_number', 'last_name', 'first_name',)
-    list_display_links = ('first_name', 'last_name', 'id',)
-    readonly_fields = ('created_by', 'created_date', 'modified_date', 'eligibility', 'package_count', 'pending_letter_count', 'letter_count')
-    fields = ('inmate_number', 'eligibility', 'last_name', 'first_name', 'package_count', 'pending_letter_count', 'letter_count', 'status',)
+    search_fields = (
+        "inmate_number",
+        "last_name",
+        "first_name",
+    )
+    list_display_links = (
+        "first_name",
+        "last_name",
+        "id",
+    )
+    readonly_fields = (
+        "created_by",
+        "created_date",
+        "modified_date",
+        "eligibility",
+        "package_count",
+        "pending_letter_count",
+        "letter_count",
+    )
+    fields = (
+        "inmate_number",
+        "eligibility",
+        "last_name",
+        "first_name",
+        "package_count",
+        "pending_letter_count",
+        "letter_count",
+        "status",
+    )
     inlines = [PersonPrisonInline]
 
     def current_prison(self, person):
         if not person.current_prison:
             return
-        link = reverse("admin:app_prison_change", kwargs={"object_id": person.current_prison.id})
+        link = reverse(
+            "admin:app_prison_change", kwargs={"object_id": person.current_prison.id}
+        )
         return format_html(f"<a href={link}>{person.current_prison.name}</a>")
 
     current_prison.allow_tags = True
@@ -132,17 +211,23 @@ class PersonAdmin(ImportExportModelAdmin):
     def pending_letter_count(self, person):
         if not person.pending_letter_count:
             return
-        return format_html(f"<a href={reverse('admin:app_letter_changelist')}?person={person.id}&workflow_stage__in=processed,awaiting_fulfillment>{person.pending_letter_count}</a>")
+        return format_html(
+            f"<a href={reverse('admin:app_letter_changelist')}?person={person.id}&workflow_stage__in={WorkflowStage.STAGE1_COMPLETE},{WorkflowStage.AWAITING_FULFILLMENT}>{person.pending_letter_count}</a>"
+        )
 
     def letter_count(self, person):
         if not person.letter_count:
             return
-        return format_html(f"<a href={reverse('admin:app_letter_changelist')}?person={person.id}>{person.letter_count}</a>")
+        return format_html(
+            f"<a href={reverse('admin:app_letter_changelist')}?person={person.id}>{person.letter_count}</a>"
+        )
 
     def package_count(self, person):
         if not person.package_count:
             return
-        return format_html(f"<a href={reverse('admin:app_letter_changelist')}?person={person.id}&workflow_stage__in=fulfilled>{person.package_count}</a>")
+        return format_html(
+            f"<a href={reverse('admin:app_letter_changelist')}?person={person.id}&workflow_stage__in={WorkflowStage.FULFILLED}>{person.package_count}</a>"
+        )
 
     def save_model(self, request, obj, form, change):
         if not obj.pk:
@@ -156,16 +241,43 @@ class PrisonAdmin(ImportExportModelAdmin):
     resource_class = PrisonResource
 
     # All mail goes through Security Processing Center, addresses suppressed
-    list_display = ('id', 'name', 'prison_type', 'display_mailing_address', 'restrictions', 'notes', 'created_by', 'created_date', 'modified_by', 'modified_date')
-    list_display_links = ('name',)
-    list_filter = ('prison_type',)
-    search_fields = ('name', 'notes', 'restrictions',)
-    fields = ('name', 'prison_type', 'mailing_address', 'mailing_city', 'mailing_state', 'mailing_zipcode', 'legacy_address', 'restrictions', 'notes',)
+    list_display = (
+        "id",
+        "name",
+        "prison_type",
+        "display_mailing_address",
+        "restrictions",
+        "notes",
+        "created_by",
+        "created_date",
+        "modified_by",
+        "modified_date",
+    )
+    list_display_links = ("name",)
+    list_filter = ("prison_type",)
+    search_fields = (
+        "name",
+        "notes",
+        "restrictions",
+    )
+    fields = (
+        "name",
+        "prison_type",
+        "mailing_address",
+        "mailing_city",
+        "mailing_state",
+        "mailing_zipcode",
+        "legacy_address",
+        "restrictions",
+        "notes",
+    )
 
     def display_mailing_address(self, prison):
         if not prison.mailing_address:
             return
-        return format_html(f"{prison.name}<br/>{prison.mailing_address}<br/>{prison.mailing_city}, {prison.mailing_state} {prison.mailing_zipcode}")
+        return format_html(
+            f"{prison.name}<br/>{prison.mailing_address}<br/>{prison.mailing_city}, {prison.mailing_state} {prison.mailing_zipcode}"
+        )
 
     display_mailing_address.allow_tags = True
     display_mailing_address.short_description = "Mailing Address"
@@ -180,13 +292,43 @@ class PrisonAdmin(ImportExportModelAdmin):
 
 @admin.register(Letter)
 class LetterAdmin(ImportExportModelAdmin):
-    list_display = ('letter_name', 'person_list_display', 'workflow_stage', 'postmark_date', 'processed_date', 'awaiting_fulfillment_date', 'fulfilled_date', 'prison_mailing_address', 'eligibility', 'created_by', 'created_date', 'modified_date')
-    list_filter = ('workflow_stage',)
-    list_display_links = ('letter_name',)
-    search_fields = ('person__last_name', 'person__first_name', 'person__inmate_number',)
-    readonly_fields = ('created_date', 'created_by',)
-    fields = ('person', 'workflow_stage', 'postmark_date', 'processed_date', 'created_date',)
-    actions = (move_to_awaiting_fulfillment, move_to_fulfilled, move_to_processed,)
+    list_display = (
+        "letter_name",
+        "person_list_display",
+        "workflow_stage",
+        "postmark_date",
+        "stage1_complete_date",
+        "awaiting_fulfillment_date",
+        "fulfilled_date",
+        "prison_mailing_address",
+        "eligibility",
+        "created_by",
+        "created_date",
+        "modified_date",
+    )
+    list_filter = ("workflow_stage",)
+    list_display_links = ("letter_name",)
+    search_fields = (
+        "person__last_name",
+        "person__first_name",
+        "person__inmate_number",
+    )
+    readonly_fields = (
+        "created_date",
+        "created_by",
+    )
+    fields = (
+        "person",
+        "workflow_stage",
+        "postmark_date",
+        "stage1_complete_date",
+        "created_date",
+    )
+    actions = (
+        move_to_awaiting_fulfillment,
+        move_to_fulfilled,
+        move_to_stage1_complete,
+    )
 
     def letter_name(self, letter):
         return f"{letter.person.last_name}, {letter.person.first_name} - {letter.postmark_date}"
@@ -195,7 +337,9 @@ class LetterAdmin(ImportExportModelAdmin):
         if not letter.person:
             # This is a problem, do something smarter here
             return
-        link = reverse('admin:app_person_change', kwargs={'object_id': letter.person.id})
+        link = reverse(
+            "admin:app_person_change", kwargs={"object_id": letter.person.id}
+        )
         return format_html(f"<a href={link}>{letter.person.last_name}</a>")
 
     person_list_display.allow_tags = True
@@ -205,16 +349,17 @@ class LetterAdmin(ImportExportModelAdmin):
         if not letter.person.current_prison:
             return
         curr_prison = letter.person.current_prison
-        return format_html(f"{letter.person.first_name} {letter.person.last_name}<br/>{letter.person.inmate_number}<br/>{curr_prison.name}<br/>{curr_prison.mailing_address}<br/>{curr_prison.mailing_city}, {curr_prison.mailing_state} {curr_prison.mailing_zipcode}")
+        # TODO: this will probably insert another line break if no additional_mailing_headers, which there are not many; fix
+        return format_html(
+            f"{letter.person.first_name} {letter.person.last_name}<br/>{letter.person.inmate_number}<br/>{curr_prison.name}<br/>{curr_prison.additional_mailing_headers}<br/>{curr_prison.mailing_address}<br/>{curr_prison.mailing_city}, {curr_prison.mailing_state} {curr_prison.mailing_zipcode}"
+        )
 
     prison_mailing_address.allow_tags = True
 
     def eligibility(self, letter):
         return letter.person.eligibility
 
-    form = make_ajax_form(Letter, {
-        'person':'person'
-        })
+    form = make_ajax_form(Letter, {"person": "person"})
 
     def get_form(self, request, obj=None, **kwargs):
         form = super().get_form(request, obj, **kwargs)
--- app/models.py
+++ app/models.py
@@ -1,13 +1,13 @@
-from enum import Enum
 from datetime import datetime, timedelta
+from enum import Enum
 
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 from django.utils.timezone import now
 
 
 class WorkflowStage(models.TextChoices):
-    PROCESSED = "processed", "Processed"
+    STAGE1_COMPLETE = "stage1_complete", "Stage 1 complete"
     AWAITING_FULFILLMENT = "awaiting_fulfillment", "Awaiting fulfillment"
     FULFILLED = "fulfilled", "Fulfilled"
     JUST_PADA = "just_pada", "Just PADA"
@@ -20,8 +20,6 @@ class PersonManager(models.Manager):
 
 
 class Person(models.Model):
-
-
     class Statuses(models.TextChoices):
         SOLITARY = "solitary", "Solitary"
         LIFER = "lifer", "Lifer"
@@ -32,13 +30,14 @@ class Person(models.Model):
     legacy_prison_id = models.SmallIntegerField(null=True)
     legacy_last_served_date = models.DateTimeField(null=True, default=None)
     notes = models.CharField(max_length=500, blank=True)
-    status = models.CharField(
-        max_length=200,
-        choices=Statuses.choices,
-        blank=True
-    )
+    status = models.CharField(max_length=200, choices=Statuses.choices, blank=True)
     created_date = models.DateTimeField(auto_now_add=True)
-    created_by=models.ForeignKey(User, null=True, related_name='person_created_by_user', on_delete=models.SET_NULL)
+    created_by = models.ForeignKey(
+        User,
+        null=True,
+        related_name="person_created_by_user",
+        on_delete=models.SET_NULL,
+    )
     modified_date = models.DateTimeField(auto_now=True)
 
     class Meta:
@@ -55,7 +54,9 @@ class Person(models.Model):
 
     @property
     def last_served(self):
-        if fulfilled_letters := self.letter_set.filter(workflow_stage__in=[WorkflowStage.FULFILLED]):
+        if fulfilled_letters := self.letter_set.filter(
+            workflow_stage__in=[WorkflowStage.FULFILLED]
+        ):
             return fulfilled_letters.order_by("fulfilled_date").first().fulfilled_date
         elif self.legacy_last_served_date:
             return datetime.date(self.legacy_last_served_date)
@@ -79,31 +80,39 @@ class Person(models.Model):
             if not self.has_pending_letters:
                 return "Eligible"
             return f"Eligible, {self.pending_letter_count} letters pending"
-        elif self.last_served <= (datetime.now().date() - timedelta(weeks=12)):
+        elif self.last_served <= (datetime.now().date() - timedelta(days=90)):
             if not self.has_pending_letters:
                 return "Eligible"
             else:
                 return f"Eligible, {self.pending_letter_count} letters pending"
-        elif date_last_fulfilled := self.letter_set.filter(workflow_stage__in=[WorkflowStage.FULFILLED]):
-            last_fulfilled_date = date_last_fulfilled.order_by(
-            "fulfilled_date"
-            ).first().fulfilled_date
+        elif date_last_fulfilled := self.letter_set.filter(
+            workflow_stage__in=[WorkflowStage.FULFILLED]
+        ):
+            last_fulfilled_date = (
+                date_last_fulfilled.order_by("fulfilled_date").first().fulfilled_date
+            )
             eligible_date = last_fulfilled_date + timedelta(weeks=12)
             return f"Eligible after {eligible_date.strftime('%B %-d, %Y')}"
         else:
-            eligibility_from_legacy_date = self.legacy_last_served_date + timedelta(weeks=12)
-            return f"Eligible after {eligibility_from_legacy_date.strftime('%B %-d, %Y')}"
+            eligibility_from_legacy_date = self.legacy_last_served_date + timedelta(
+                days=90
+            )
+            return (
+                f"Eligible after {eligibility_from_legacy_date.strftime('%B %-d, %Y')}"
+            )
 
     @property
     def package_count(self):
-        return self.letter_set.filter(workflow_stage__in=[WorkflowStage.FULFILLED]).count()
+        return self.letter_set.filter(
+            workflow_stage__in=[WorkflowStage.FULFILLED]
+        ).count()
 
     @property
     def pending_letter_count(self):
         return self.letter_set.filter(
             workflow_stage__in=[
-                WorkflowStage.PROCESSED,
-                WorkflowStage.AWAITING_FULFILLMENT
+                WorkflowStage.STAGE1_COMPLETE,
+                WorkflowStage.AWAITING_FULFILLMENT,
             ]
         ).count()
 
@@ -111,8 +120,8 @@ class Person(models.Model):
     def pending_letters(self):
         return self.letter_set.filter(
             workflow_stage__in=[
-                WorkflowStage.PROCESSED,
-                WorkflowStage.AWAITING_FULFILLMENT
+                WorkflowStage.STAGE1_COMPLETE,
+                WorkflowStage.AWAITING_FULFILLMENT,
             ]
         ).all()
 
@@ -131,56 +140,72 @@ class Person(models.Model):
 
 
 class Prison(models.Model):
-
-
     class PrisonTypes(models.TextChoices):
         SCI = "sci", "SCI"
         FCI = "fci", "FCI"
         CITY = "city", "City"
         COUNTY = "county", "County"
         FDC = "fdc", "FDC"
-        tMMIGRATION_DETENTION = "immigration_detention", "Immigration Detention Facility"
+        tMMIGRATION_DETENTION = (
+            "immigration_detention",
+            "Immigration Detention Facility",
+        )
         BOOT_CAMP = "boot_camp", "Boot Camp"
 
-
     name = models.CharField(max_length=200)
-    prison_type = models.CharField(
-        max_length=200,
-        choices=PrisonTypes.choices
-    )
+    prison_type = models.CharField(max_length=200, choices=PrisonTypes.choices)
     legacy_address = models.CharField(max_length=200, blank=True)
+    additional_mailing_headers = models.CharField(max_length=200, blank=True)
     mailing_address = models.CharField(max_length=200, blank=True)
     mailing_city = models.CharField(max_length=200, blank=True)
     mailing_state = models.CharField(max_length=50, blank=True)
     mailing_zipcode = models.CharField(max_length=200, blank=True)
     restrictions = models.CharField(max_length=200, blank=True)
+    accepts_books = models.CharField(
+        max_length=200, choices=["True", "False", "Unknown"]
+    )
     legacy_id = models.CharField(max_length=50, unique=True, blank=True)
     notes = models.CharField(max_length=200, blank=True)
     created_date = models.DateTimeField(auto_now_add=True)
-    created_by=models.ForeignKey(User, null=True, related_name='prison_created_by_user', on_delete=models.SET_NULL)
+    created_by = models.ForeignKey(
+        User,
+        null=True,
+        related_name="prison_created_by_user",
+        on_delete=models.SET_NULL,
+    )
     modified_date = models.DateTimeField(auto_now=True)
     # not sure about this, only implemented here currently, see save method on PrisonAdmin
-    modified_by=models.ForeignKey(User, null=True, related_name='prison_modified_by_user', on_delete=models.SET_NULL)
+    modified_by = models.ForeignKey(
+        User,
+        null=True,
+        related_name="prison_modified_by_user",
+        on_delete=models.SET_NULL,
+    )
 
     def __str__(self):
         return self.name
 
 
 class Letter(models.Model):
-    person = models.ForeignKey('Person', on_delete=models.CASCADE)
+    person = models.ForeignKey("Person", on_delete=models.CASCADE)
     postmark_date = models.DateField(null=True, blank=True)
-    processed_date = models.DateField(null=True, blank=True, default=now)
+    stage1_complete_date = models.DateField(null=True, blank=True, default=now)
     awaiting_fulfillment_date = models.DateField(null=True, blank=True)
     fulfilled_date = models.DateField(null=True, blank=True)
     # change to choices
     workflow_stage = models.CharField(
         max_length=200,
         choices=WorkflowStage.choices,
-        default=WorkflowStage.PROCESSED
+        default=WorkflowStage.STAGE1_COMPLETE,
     )
     notes = models.CharField(max_length=200, blank=True)
     created_date = models.DateTimeField(auto_now_add=True)
-    created_by=models.ForeignKey(User, null=True, related_name='letter_created_by_user', on_delete=models.SET_NULL)
+    created_by = models.ForeignKey(
+        User,
+        null=True,
+        related_name="letter_created_by_user",
+        on_delete=models.SET_NULL,
+    )
     modified_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
@@ -188,11 +213,20 @@ class Letter(models.Model):
 
 
 class PersonPrison(models.Model):
-    person = models.ForeignKey('Person', on_delete=models.CASCADE, related_name='prisons')
-    prison = models.ForeignKey('Prison', on_delete=models.CASCADE, related_name='people')
+    person = models.ForeignKey(
+        "Person", on_delete=models.CASCADE, related_name="prisons"
+    )
+    prison = models.ForeignKey(
+        "Prison", on_delete=models.CASCADE, related_name="people"
+    )
     current = models.BooleanField(default=True)
     created_date = models.DateTimeField(auto_now_add=True)
-    created_by=models.ForeignKey(User, null=True, related_name='personprison_created_by_user', on_delete=models.SET_NULL)
+    created_by = models.ForeignKey(
+        User,
+        null=True,
+        related_name="personprison_created_by_user",
+        on_delete=models.SET_NULL,
+    )
     modified_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
