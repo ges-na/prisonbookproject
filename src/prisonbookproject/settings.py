9400d16884ce6b9be25e400561603997d023205b
ges-na <gesina.a.phillips@gmail.com>
2025-08-17 13:58:49 -0400

    fixing bad tag spacing

--- src/prisonbookproject/settings.py
+++ src/prisonbookproject/settings.py
@@ -144,5 +144,3 @@ STATICFILES_FINDERS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-EXPORT_VARS = ["ENV_NAME"]
