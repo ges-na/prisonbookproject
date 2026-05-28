0c9f04b13f4c6c15300c3c76b55382a92e3e87d7
ges-na <gesina.a.phillips@gmail.com>
2026-05-27 22:20:09 -0400

    is_contributor admin action | set prod machine scaling

--- src/auth/admin.py
+++ src/auth/admin.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 
 from src.auth.models import User
@@ -6,7 +6,7 @@ from src.auth.models import User
 
 class UserAdmin(DjangoUserAdmin):
     search_fields = ("email", "last_name", "first_name")
-    ordering = ("date_joined",)
+    ordering = ("-date_joined",)
     add_fieldsets = (
         (None, {"fields": ("email", "password1", "password2")}),
         (("Personal info"), {"fields": ("first_name", "last_name")}),
@@ -39,6 +39,21 @@ class UserAdmin(DjangoUserAdmin):
         "is_staff",
         "is_superuser",
     ]
+    actions = ("give_is_contributor_permission",)
+
+    @admin.action(description="Give users contributor permissions")
+    def give_is_contributor_permission(self, request, queryset):
+        change = []
+        for user in queryset:
+            if not user.is_active:
+                messages.error(
+                    request,
+                    "User f{user.email} is not currently active, cannot give contributor permission.",
+                )
+                continue
+            else:
+                change.append(user.id)
+        queryset.filter(id__in=change).update(is_contributor=True)
 
 
 admin.site.register(User, UserAdmin)
