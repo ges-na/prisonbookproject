0bc2ab3fb84936915b010f638092832e8ac91966
ges-na <gesina.a.phillips@gmail.com>
2026-06-28 17:08:56 -0400

    update

--- trigger-backup.sh
+++ trigger-backup.sh
@@ -1,1 +1,14 @@
-flyctl machine run -a ppbb-db-backup-worker --region=iad --restart=no --rm ghcr.io/significa/fly-pg-dump-to-s3
+set -eo pipefail
+
+if [[ -z "$FLY_APP" || -z "$FLY_API_TOKEN" ]]; then
+  >&2 echo "Env vars FLY_APP and FLY_API_TOKEN must not be empty"
+  exit 1
+fi
+
+echo "Starting machine with volume $volume_id"
+flyctl machine run \
+    --app="ppbp-db-backup-worker" \
+    --region="iad" \
+    --restart=no \
+    --rm \
+    "ghcr.io/significa/fly-pg-dump-to-s3" \
