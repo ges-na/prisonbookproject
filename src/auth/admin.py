6a1e232b7b0c0652aeb6a82f22486bee56dc783a
ges-na <gesina.a.phillips@gmail.com>
2026-06-28 18:49:38 -0400

    adding admin actions for custom user management permissions

--- src/auth/admin.py
+++ src/auth/admin.py
@@ -43,27 +43,28 @@ class UserAdmin(DjangoUserAdmin):
         "is_superuser",
     ]
     actions = (
-        "change_is_contributor_permission",
-        "change_is_active_permission",
-        "change_is_staff_permission",
+        "enable_is_active",
+        "enable_is_contributor",
+        "enable_is_staff",
+        "disable_is_active",
+        "disable_is_contributor",
+        "disable_is_staff",
     )
     readonly_fields = ("last_login", "date_joined")
 
-    @admin.action(description="Change user's active status")
-    def change_is_active_permission(self, request, queryset):
-        if "CustomAuth.change_is_active" not in request.user.get_all_permissions():
-            messages.error(request, "You do not have permission to change active status")
-            return
-        change = []
-        for user in queryset:
-            change.append(user.id)
-        queryset.filter(id__in=change).update(is_active=True)
+    @admin.action(description="Enable user's active status", permissions=["change_is_active"])
+    def enable_is_active(self, request, queryset):
+        queryset.update(is_active=True)
 
-    @admin.action(description="Change user's contributor status")
-    def change_is_contributor_permission(self, request, queryset):
-        if "CustomAuth.change_is_contrib" not in request.user.get_all_permissions():
-            messages.error(request, "You do not have permission to change contributor status")
-            return
+    @admin.action(description="Disable user's active status", permissions=["change_is_active"])
+    def disable_is_active(self, request, queryset):
+        queryset.update(is_active=False)
+
+    def has_change_is_active_permission(self, request):
+        return bool("CustomAuth.change_is_active" in request.user.get_all_permissions())
+
+    @admin.action(description="Enable user's contributor status", permissions=["change_is_contrib"])
+    def enable_is_contributor(self, request, queryset):
         change = []
         for user in queryset:
             if not user.is_active:
@@ -76,10 +77,17 @@ class UserAdmin(DjangoUserAdmin):
                 change.append(user.id)
         queryset.filter(id__in=change).update(is_contributor=True)
 
-    def change_is_staff_permission(self, request, queryset):
-        if "CustomAuth.change_is_staff" not in request.user.get_all_permissions():
-            messages.error(request, "You do not have permission to change staff status")
-            return
+    @admin.action(
+        description="Disable user's contributor status", permissions=["change_is_contrib"]
+    )
+    def disable_is_contributor(self, request, queryset):
+        queryset.update(is_contributor=False)
+
+    def has_change_is_contrib_permission(self, request):
+        return bool("CustomAuth.change_is_contrib" in request.user.get_all_permissions())
+
+    @admin.action(description="Enable user's staff status", permissions=["change_is_staff"])
+    def enable_is_staff(self, request, queryset):
         change = []
         for user in queryset:
             if not user.is_active:
@@ -91,6 +99,13 @@ class UserAdmin(DjangoUserAdmin):
             else:
                 change.append(user.id)
         queryset.filter(id__in=change).update(is_staff=True)
+
+    @admin.action(description="Disable user's staff status", permissions=["change_is_staff"])
+    def disable_is_staff(self, request, queryset):
+        queryset.update(is_staff=False)
+
+    def has_change_is_staff_permission(self, request):
+        return bool("CustomAuth.change_is_staff" in request.user.get_all_permissions())
 
 
 admin.site.register(User, UserAdmin)
